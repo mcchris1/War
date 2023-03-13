1ac95ba5d18b209c4cee3efb943c7d9f86a3f450
michae.lchristenson726@yahoo.com <121463561+mcchris1@users.noreply.github.com>
2023-03-13 11:39:24 -0400

    identifies objects, actions

---
+++ script.js
@@ -0,0 +1,49 @@
+//Identify objects, actions, variables  
+
+const SUITS = ['♠', '♥', '♦', '♣'] 
+const RANKS = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+const VALUES = {
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
+    "10": 10, 
+    "J": 11,
+    "Q": 12,
+    "K": 13,
+    "A": 14,
+}
+
+class Card {
+    constructor(suit, rank, value){
+        this.suit = suit;
+        this.rank = rank;
+        this.value = value;
+    }
+}
+
+class Deck {
+    constructor(){
+        this.cards = cards;
+        this.freshDeck()
+    }
+    makeDeck() {
+        
+    }
+    shuffle() {
+       
+    }
+    
+}
+
+let weDeck
+let theyDeck
+
+startGame()
+function startGame() {
+    
+}
